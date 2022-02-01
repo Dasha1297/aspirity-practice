9b8dbe2032b49385a0bbfdef62c1a4dde3943cd9
Dasha Lukina <dashalukina1297@gmail.com>
2022-02-01 12:49:26 +0700

    ready for working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import Example from "./components/Example/Example";
 
 function App() {
-  return (
-    <div>
-      <Example />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
