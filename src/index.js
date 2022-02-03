fe467b58e29fc8841a775ed78e3bc2627e010c53
Dasha Lukina <dashalukina1297@gmail.com>
2022-02-03 20:49:34 +0700

    fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App"; HEAD
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
-import {store} from "./redux/consts";
+import { Provider } from "react-redux";
+import { store } from "./redux/consts";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider>
       <App />
-    </Provider>
-    
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/consts.js
+++ src/redux/consts.js
@@ -1,10 +1,7 @@
-
-export const ApiUrl = "http://173.212.214.70:3002/";
-
-import { createStore } from 'redux';
-import { projectReducer } from './projectReducer';
+import { createStore } from "redux";
+import { projectReducer } from "./projectReducer";
 
 const rootReducer = projectReducer;
 
-export const store = createStore(rootReducer)
-
+export const ApiUrl = "http://173.212.214.70:3002/";
+export const store = createStore(rootReducer);
