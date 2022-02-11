114c6ee5218dc9bb6b53a1b5fba7fad31e0d74d6
Dasha Lukina <dashalukina1297@gmail.com>
2022-02-11 15:35:16 +0700

    fix api

--- src/redux/consts.js
+++ src/redux/consts.js
@@ -1,4 +1,4 @@
-export const ApiUrl = "http://173.212.214.70:3002/";
+export const ApiUrl = "https://173.212.214.70:3002/";
 export const REFRESH_TOKEN = "REFRESH_TOKEN";
 
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
@@ -7,11 +7,9 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 
-
 export const ADD_BOARD = "ADD_BOARD";
 export const DELETE_BOARD = "DELETE_BOARD";
 export const EDIT_BOARD = "EDIT_BOARD";
 
 export const ADD_PROJECTS = "ADD_PROJECTS";
 export const REMOVE_PROJECTS = "REMOVE_PROJECTS";
-
