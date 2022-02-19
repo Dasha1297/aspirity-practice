0d473bdf93f8e5cc1feb14b453e97e6c9d9f2fbf
Ursula95 <ursula95ursula95@gmail.com>
2022-02-19 19:04:33 +0700

    join logic getBoard, addBoards

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -1,27 +1,41 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Board from "./Board/Board";
 import style from "./Boards.module.css";
 import plus from "../../assets/plus.svg";
 import AddBoard from "./AddBoard";
 import Modal from "../Modal/Modal";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import { useDispatch } from "react-redux";
+import { getBoards } from "../../redux/actions/boardsActions";
 
 const Boards = () => {
+  const dispatch = useDispatch();
   const boards = useSelector((state) => state.boardReducer.boards);
   const Boards = boards.map((board) => <Board name={board.name} users={12} />);
 
-  const addBoard = () => {};
+  const [modalAddBoardActive, setModalAddBoardActive] = useState(false);
+
+  useEffect(() => {
+    dispatch(getBoards());
+  }, [dispatch]);
+
   return (
     <div className={style.wrapper}>
       <h1>Все доски</h1>
       <div className={style.boards}>
-        <button className={style.add__button} onClick={() => addBoard()}>
+        <button className={style.add__button} onClick={() => setModalAddBoardActive(true)}>
           <img src={plus} alt='plus' />
           Создать новую доску
         </button>
         {Boards}
       </div>
+      <Modal
+        active={modalAddBoardActive}
+        setActive={setModalAddBoardActive}
+      >
+        <AddBoard />
+      </Modal>
     </div>
   );
 };
