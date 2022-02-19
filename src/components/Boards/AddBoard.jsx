90589aedf6cf4bf8a1999a4d628143147fd6b460
Ursula95 <ursula95ursula95@gmail.com>
2022-02-19 19:12:45 +0700

    join logic addBoards

--- src/components/Boards/AddBoard.jsx
+++ src/components/Boards/AddBoard.jsx
@@ -4,17 +4,31 @@ import Button from "../UI/Button/Button";
 import InputField from "../UI/InputField/InputField";
 import Board from "./Board/Board";
 import style from "./Boards.module.css";
+
 const AddBoard = () => {
+  const [name, setName] = useState("");
+
   const dispatch = useDispatch();
 
-  const addButton = () => {
-    dispatch(addBoard({ name: "Board", id: 5 }));
+  const addBoard = (event) => {
+    event.preventDefault();
+
+    dispatch(addBoard({ name, id: 5 }));
+    setName("");
+    
   };
 
   return (
     <div className={style.create__board}>
-      <InputField />
-      <Button onClick={() => addButton()} />
+      <Form name={"Новый проект"}>
+      <InputField
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder={"Название доски"}
+        type={"text"}
+      />
+      <Button type='button' text={"Сохранить"} onClick={addBoard} />
+    </Form>
     </div>
   );
 };
